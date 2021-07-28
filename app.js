722e2a3885ae9bb4ad35cfbf08b1c9bc35217344
Ayz Abdulwahid <ayz.abdulwahid@gmail.com>
2021-07-28 17:45:52 +0800

    moved js code from index.html to app.js

--- app.js
+++ app.js
@@ -1,15 +1,64 @@
-let box1, box2, box3, box4, box5, box6, box7, box8;
+let arr = [];
+//document.getElementbyId
+const box1 = document.getElementById("box1");
+const box2 = document.getElementById("box2");
+const box3 = document.getElementById("box3");
+const box4 = document.getElementById("box4");
+const box5 = document.getElementById("box5");
+const box6 = document.getElementById("box6");
+const box7 = document.getElementById("box7");
+const box8 = document.getElementById("box8");
+
+//addEventListener
+box1.addEventListener("click", editBox, { once: true });
+box2.addEventListener("click", editBox, { once: true });
+box3.addEventListener("click", editBox, { once: true });
+box4.addEventListener("click", editBox, { once: true });
+box5.addEventListener("click", editBox, { once: true });
+box6.addEventListener("click", editBox, { once: true });
+box7.addEventListener("click", editBox, { once: true });
+box8.addEventListener("click", editBox, { once: true });
+
+function editBox(event) {
+  //getting box id
+  event.preventDefault();
+  console.log("clicked");
+  //creating text box
+  try {
+    event.target.innerHTML = `
+          <input id="num-input" maxlength="1"></input>
+          <button id="ok-button" type="submit">OK</button>`;
+    let num = document.getElementById("num-input");
+    let btn = document.getElementById("ok-button");
+    btn.addEventListener("click", () => {
+      if (num.value <= 8 && num.value >= 1) {
+        addToArr(event.target.id, num.value);
+        event.target.innerHTML = num.value;
+        btn.removeEventListener("click", () => {
+          console.log("removed");
+        });
+      } else {
+        alert("Please enter 1-8 only");
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function addToArr(boxNum, boxVal) {
+  const found = arr.some((el) => el.id === boxNum);
+  if (!found) {
+    arr = [...arr, { id: boxNum, value: boxVal }];
+  } else {
+    console.log("box already exist");
+  }
+  arr.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0));
+  console.log(arr);
+}
 
 const checkingAnswer = () => {
   //box1 value is not equals to +- the value of box2 and box4
-  box1 = 5;
-  box2 = 3;
-  box4 = 8;
-  box3 = 2;
-  box5 = 1;
-  box6 = 7;
-  box7 = 6;
-  box8 = 4;
   if (
     box1 - 1 != box2 &&
     box1 + 1 != box2 &&
@@ -80,5 +129,3 @@ const checkingAnswer = () => {
     console.log("box 8 is correct");
   }
 };
-
-checkingAnswer();
