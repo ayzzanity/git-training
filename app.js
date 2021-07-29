4de1fe61cb45b96a6307132a18c53cb4f14f76b8
Ayz Abdulwahid <ayz.abdulwahid@gmail.com>
2021-07-29 15:49:32 +0800

    turned html related functions to a module

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ let arr = [
   { id: "box7", value: "" },
   { id: "box8", value: "" },
 ];
-let input;
 let currentId;
 
 //document.getElementbyId
@@ -22,82 +21,82 @@ const box7 = document.getElementById("box7");
 const box8 = document.getElementById("box8");
 
 //addEventListener
-box1.addEventListener("click", editBox);
-box2.addEventListener("click", editBox);
-box3.addEventListener("click", editBox);
-box4.addEventListener("click", editBox);
-box5.addEventListener("click", editBox);
-box6.addEventListener("click", editBox);
-box7.addEventListener("click", editBox);
-box8.addEventListener("click", editBox);
+box1.addEventListener("click", htmlFn().editBox);
+box2.addEventListener("click", htmlFn().editBox);
+box3.addEventListener("click", htmlFn().editBox);
+box4.addEventListener("click", htmlFn().editBox);
+box5.addEventListener("click", htmlFn().editBox);
+box6.addEventListener("click", htmlFn().editBox);
+box7.addEventListener("click", htmlFn().editBox);
+box8.addEventListener("click", htmlFn().editBox);
 
-//EDITABLE BOX
-function editBox(event) {
-  //temporarily removing event listener on box
-  event.target.removeEventListener("click", editBox);
-  event.preventDefault();
-  //check what box num
-  if (currentId != undefined && currentId != event.target.id) {
+function htmlFn() {
+  //EDITABLE BOX
+  function editBox(event) {
+    //temporarily removing event listener on box
+    event.target.removeEventListener("click", editBox);
+    event.preventDefault();
+    //check what box num
+    if (currentId != undefined && currentId != event.target.id) {
+      try {
+        let currentBox = document.getElementById(currentId);
+        currentBox.innerHTML = getBoxValue(currentId);
+        currentBox.addEventListener("click", editBox);
+        currentId = event.target.id;
+      } catch (e) {}
+    }
+    //setting new box as current id
+    currentId = event.target.id;
+    //creating text box
     try {
-      let currentBox = document.getElementById(currentId);
-      currentBox.innerHTML = getBoxValue(currentId);
-      currentBox.addEventListener("click", editBox);
-      currentId = event.target.id;
-    } catch (e) {}
-  }
-
-  //creating text box
-  currentId = event.target.id;
-  try {
-    event.target.innerHTML = `
+      event.target.innerHTML = `
           <input id="num-input" maxlength="1"></input>
           <button id="ok-button" type="submit">OK</button>`;
-    //button and txt input elements
-    let num = document.getElementById("num-input");
-    let btn = document.getElementById("ok-button");
-    //btn listener
-    btn.addEventListener("click", () => {
-      console.clear();
-      //checking if num is 1-8
-      if (num.value <= 8 && num.value >= 0) {
-        //checking if num already in the box / array
-        input = num.value;
-
-        //adding to array and updating box, restoring event listener
-        (async () => {
-          await addToArr(currentId, num.value);
-        })();
-        event.target.innerHTML = num.value;
-        event.target.addEventListener("click", editBox);
-        //checking answer
-        checkingAnswer(arr);
-      } else {
-        alert("Please enter 1-8 only");
-      }
-    });
-  } catch (e) {}
-}
-function getBoxValue(boxNum) {
-  let val;
-  arr.forEach((obj) => {
-    if (obj.id === boxNum) {
-      val = obj.value;
-    }
-  });
-  return val;
-}
-//ADDING BOX NUM AND VALUE TO ARRAY
-function addToArr(boxNum, boxVal) {
-  //update value for existing box
-  return new Promise((resolve, reject) => {
+      //button and txt input elements
+      let num = document.getElementById("num-input");
+      let btn = document.getElementById("ok-button");
+      //btn listener
+      btn.addEventListener("click", () => {
+        console.clear();
+        //checking if num is 1-8
+        if (num.value <= 8 && num.value >= 0) {
+          //adding to array and updating box, restoring event listener
+          (async () => {
+            await addToArr(currentId, num.value);
+          })();
+          event.target.innerHTML = num.value;
+          event.target.addEventListener("click", editBox);
+          //checking answer
+          checkingAnswer(arr);
+        } else {
+          alert("Please enter 1-8 only");
+        }
+      });
+    } catch (e) {}
+  }
+  function getBoxValue(boxNum) {
+    let val;
     arr.forEach((obj) => {
       if (obj.id === boxNum) {
-        obj.value = boxVal;
+        val = obj.value;
       }
     });
-    //console.log(arr);
-    resolve(console.log(arr));
-  });
+    return val;
+  }
+  //ADDING BOX NUM AND VALUE TO ARRAY
+  function addToArr(boxNum, boxVal) {
+    //update value for existing box
+    return new Promise((resolve, reject) => {
+      arr.forEach((obj) => {
+        if (obj.id === boxNum) {
+          obj.value = boxVal;
+        }
+      });
+      //console.log(arr);
+      resolve(console.log(arr));
+    });
+  }
+  return { editBox };
 }
 
 const checkingAnswer = (data) => {
@@ -457,5 +456,3 @@ const checkingAnswer = (data) => {
     console.log("You are a loser");
   }
 };
-
-//checkingAnswer(arr);
