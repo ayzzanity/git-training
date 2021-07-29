f68dd11625aed40976f3ad9f702025ba4bfee010
Ayz Abdulwahid <ayz.abdulwahid@gmail.com>
2021-07-29 09:38:27 +0800

    fixed box UI and js

--- app.js
+++ app.js
@@ -10,49 +10,65 @@ const box7 = document.getElementById("box7");
 const box8 = document.getElementById("box8");
 
 //addEventListener
-box1.addEventListener("click", editBox, { once: true });
-box2.addEventListener("click", editBox, { once: true });
-box3.addEventListener("click", editBox, { once: true });
-box4.addEventListener("click", editBox, { once: true });
-box5.addEventListener("click", editBox, { once: true });
-box6.addEventListener("click", editBox, { once: true });
-box7.addEventListener("click", editBox, { once: true });
-box8.addEventListener("click", editBox, { once: true });
+box1.addEventListener("click", editBox);
+box2.addEventListener("click", editBox);
+box3.addEventListener("click", editBox);
+box4.addEventListener("click", editBox);
+box5.addEventListener("click", editBox);
+box6.addEventListener("click", editBox);
+box7.addEventListener("click", editBox);
+box8.addEventListener("click", editBox);
 
+//EDITABLE BOX
 function editBox(event) {
-  //getting box id
+  //temporarily removing event listener on box
+  event.target.removeEventListener("click", editBox);
   event.preventDefault();
-  console.log("clicked");
   //creating text box
   try {
     event.target.innerHTML = `
           <input id="num-input" maxlength="1"></input>
           <button id="ok-button" type="submit">OK</button>`;
+    //button and txt input elements
     let num = document.getElementById("num-input");
     let btn = document.getElementById("ok-button");
+    //btn listener
     btn.addEventListener("click", () => {
+      //checking if num is 1-8
       if (num.value <= 8 && num.value >= 1) {
-        addToArr(event.target.id, num.value);
-        event.target.innerHTML = num.value;
-        btn.removeEventListener("click", () => {
-          console.log("removed");
-        });
+        //checking if num already in the box / array
+        let numFound = arr.some((el) => el.value === num.value);
+        if (!numFound) {
+          //adding to array and updating box, restoring event listener
+          addToArr(event.target.id, num.value);
+          event.target.innerHTML = num.value;
+          event.target.addEventListener("click", editBox);
+        } else {
+          alert("Number already exists!");
+        }
       } else {
         alert("Please enter 1-8 only");
       }
     });
-  } catch (e) {
-    console.log(e);
-  }
+  } catch (e) {}
 }
 
+//ADDING BOX NUM AND VALUE TO ARRAY
 function addToArr(boxNum, boxVal) {
-  const found = arr.some((el) => el.id === boxNum);
-  if (!found) {
+  //checking if box already exists in array
+  const boxFound = arr.some((el) => el.id === boxNum);
+  //add to array if box not found
+  if (!boxFound) {
     arr = [...arr, { id: boxNum, value: boxVal }];
   } else {
-    console.log("box already exist");
+    //update value for existing box
+    arr.forEach((obj) => {
+      if (obj.id === boxNum) {
+        obj.value = boxVal;
+      }
+    });
   }
+  //sort box by number for easier checking
   arr.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0));
   console.log(arr);
 }
